5ec268d2a6d2d1fdc0fb2c959a85383b097642f2
Hagin <hagincubes@gmail.com>
2017-02-10 17:26:36 -0500

    The app works fully and will add more features very soon.

---
+++ SpaceGameReloaded/GameViewController.swift
@@ -0,0 +1,55 @@
+//
+//  GameViewController.swift
+//  SpaceGameReloaded
+//
+//  Created by Christopher Onyango on 2/8/17.
+//  Copyright © 2017 Hagin Onyango. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'MenuScene.sks'
+            if let scene = SKScene(fileNamed: "MenuScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ SpaceGameReloaded/MenuScene.swift
@@ -0,0 +1,76 @@
+//
+//  MenuScene.swift
+//  SpaceGameReloaded
+//
+//  Created by Christopher Onyango on 2/9/17.
+//  Copyright © 2017 Hagin Onyango. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MenuScene: SKScene {
+    
+    var starfield:SKEmitterNode!
+    
+    var newGameButtonNode:SKSpriteNode!
+    var difficultyButtonNode:SKSpriteNode!
+    var difficultyLabelNode:SKLabelNode!
+    
+    override func didMove(to view: SKView) {
+        
+        
+        starfield = self.childNode(withName: "starfield") as! SKEmitterNode
+        starfield.advanceSimulationTime(10)
+        
+        newGameButtonNode = self.childNode(withName: "newGameButton") as! SKSpriteNode
+        difficultyButtonNode = self.childNode(withName: "difficultyButton") as! SKSpriteNode
+        
+        newGameButtonNode.texture = SKTexture(imageNamed: "newGameButton")
+        difficultyButtonNode.texture = SKTexture(imageNamed: "difficultyButton")
+        
+        difficultyLabelNode = self.childNode(withName: "difficultyLabel") as! SKLabelNode
+        
+        let userDefaults = UserDefaults.standard
+        
+        if userDefaults.bool(forKey: "hard"){
+            difficultyLabelNode.text = "Hard"
+        } else {
+            difficultyLabelNode.text = "Easy"
+        }
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first
+        if let location = touch?.location(in: self) {
+            let nodesArray = self.nodes(at: location)
+            
+            if nodesArray.first?.name == "newGameButton" {
+                let transition = SKTransition.flipHorizontal(withDuration: 0.5)
+                let gameScene = GameScene(size: self.size)
+                self.view!.presentScene(gameScene, transition: transition)
+            } else if nodesArray.first?.name == "difficultyButton" {
+                changeDifficulty()
+            }
+        }
+        
+    }
+    
+    func changeDifficulty() {
+        let userDefaults = UserDefaults.standard
+        
+        if difficultyLabelNode.text == "Easy" {
+            difficultyLabelNode.text = "Hard"
+            userDefaults.set(true, forKey: "hard")
+        } else {
+            difficultyLabelNode.text = "Easy"
+            userDefaults.set(false, forKey: "hard")
+        }
+        
+        userDefaults.synchronize()
+    }
+    
+    
+}
